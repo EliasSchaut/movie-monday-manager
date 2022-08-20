b9aa2f672566dadf99fa35b80d2b532ac3e7401e
EliasSchaut <eschaut@outlook.de>
2022-08-20 12:28:30 +0200

    implement login route

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,14 +1,13 @@
 import { Injectable } from '@nestjs/common'
 import { UsersService } from '../users/users.service'
-import {Prisma} from "@prisma/client"
 const bcrypt = require('bcrypt')
 
 @Injectable()
 export class AuthService {
     constructor(private usersService: UsersService) {}
 
-    async validateUser(email: Prisma.UserWhereUniqueInput, pass: string): Promise<any> {
-        const user = await this.usersService.get(email);
+    async validateUser(email: string, pass: string): Promise<any> {
+        const user = await this.usersService.get({email: email})
         if (user && await bcrypt.compare(pass, user.password)) {
             const { password, ...result } = user;
             return result;
