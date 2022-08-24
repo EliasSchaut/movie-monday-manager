66303abc1d5be4c52758715f970b04b95c86fb1e
EliasSchaut <eschaut@outlook.de>
2022-08-24 21:32:28 +0200

    add vue framework

---
+++ client/src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <nav>
+        <router-link to="/">Home</router-link>
+        <router-link to="/about">About</router-link>
+      </nav>
+    </div>
+  </header>
+
+  <router-view />
+</template>
+
+<style scoped>
+
+</style>
