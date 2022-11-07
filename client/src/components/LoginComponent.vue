fa3a79c62f0b9eeecc174c4fa04fb78509461cd3
EliasSchaut <eschaut@outlook.de>
2022-11-07 19:08:47 +0100

    client: implement form validation

--- client/src/components/LoginComponent.vue
+++ client/src/components/LoginComponent.vue
@@ -3,19 +3,37 @@
   <div class="form-intro">
     <p class="big"><b>{{ head[route] }}</b></p>
   </div>
-  <form :action="route_base + route" @submit.prevent="onSubmit" id="form_register" class="form needs-validation">
+  <form :action="route_base + route" @submit.prevent="onSubmit" id="form_register" class="form was-validated" novalidate>
     <div class="mb-3" v-if="route === 'register'">
       <label for="form_name" class="form-label">{{ form.name }}</label>
-      <input type="text" class="form-control" id="form_name" placeholder="Max Mustermann" name="name" required>
+      <input type="text" class="form-control" id="form_name" placeholder="Max Mustermann" name="name" pattern="^[A-Z](.*)$" required>
+      <div class="valid-feedback">
+        Looks good!
+      </div>
+      <div class="invalid-feedback">
+        Should start with a capital letter.
+      </div>
     </div>
     <div class="mb-3">
       <label for="form_username" class="form-label">{{ form.username }}</label>
-      <input type="text" class="form-control" id="form_username" placeholder="max@mustermann.de" name="username" required>
+      <input type="text" class="form-control" id="form_username" placeholder="max@mustermann.de" name="username" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" required>
+      <div class="valid-feedback">
+        Looks good!
+      </div>
+      <div class="invalid-feedback">
+        Please enter a valid email address!
+      </div>
     </div>
     <div class="mb-3">
       <label for="form_password" class="form-label">{{ form.password }}</label>
-      <input type="password" class="form-control" id="form_password" placeholder="••••••••••••••••" name="password"
-             required>
+      <input type="password" class="form-control" id="form_password" placeholder="•••" name="password"
+             pattern="^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$" required>
+      <div class="valid-feedback">
+        Looks good!
+      </div>
+      <div class="invalid-feedback">
+        Minimum eight characters, at least one letter and one number!
+      </div>
     </div>
     <button v-if="!loading.value" id="button_submit" type="submit" class="btn btn-primary form-submit"
             data-bs-placement="bottom">
@@ -48,7 +66,7 @@ export default {
         password: "Password",
         name: "Name",
         submit: {
-          name: "Bestätigen",
+          name: "Submit",
           loading: "Fertigstellen..."
         }
       }
@@ -65,6 +83,10 @@ export default {
   methods: {
     async onSubmit(e: SubmitEvent) {
       const form_html = e.target as HTMLFormElement;
+      if (!form_html.checkValidity()) {
+        return
+      }
+
       const form = new FormData(form_html);
       const post = {} as any;
       form.forEach((value, key) => {
@@ -80,11 +102,12 @@ export default {
       })
         .then((res) => res.json())
         .then((data) => {
-          console.log(data);
           if (data.hasOwnProperty("access_token")) {
             localStorage.setItem("access_token", data.access_token);
+            router.push("/");
+          } else {
+            router.push("../login");
           }
-          router.push("/");
         });
     }
   }
