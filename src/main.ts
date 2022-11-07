4e5bb3c362b1d698f9ae195b9e11de955889dc99
EliasSchaut <eschaut@outlook.de>
2022-11-07 19:26:33 +0100

    common: add port configuration

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   app.use(helmet());
-  await app.listen(3000);
+  await app.listen(process.env.PORT as string);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
