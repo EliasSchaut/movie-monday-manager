318f01a84bfe505c49c92b69a494f76ea6b81af1
EliasSchaut <eschaut@outlook.de>
2022-11-09 18:43:00 +0100

    client: update login regex + add password confirm

--- client/src/components/LoginComponent.vue
+++ client/src/components/LoginComponent.vue
@@ -3,7 +3,7 @@
   <div class="form-intro">
     <p class="big"><b>{{ head[route] }}</b></p>
   </div>
-  <form :action="route_base + route" @submit.prevent="onSubmit" id="form_register" class="form was-validated" novalidate>
+  <form :action="route_base + route" @submit.prevent="onSubmit" id="form_register" class="form was-validated">
     <div class="mb-3" v-if="route === 'register'">
       <label for="form_name" class="form-label">{{ form.name }}</label>
       <input type="text" class="form-control" id="form_name" placeholder="Max Mustermann" name="name" pattern="^[A-Z](.*)$" required>
@@ -16,7 +16,7 @@
     </div>
     <div class="mb-3">
       <label for="form_username" class="form-label">{{ form.username }}</label>
-      <input type="text" class="form-control" id="form_username" placeholder="max@mustermann.de" name="username" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" required>
+      <input type="text" class="form-control" id="form_username" placeholder="max@mustermann.de" name="username" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,7}$" required>
       <div class="valid-feedback">
         Looks good!
       </div>
@@ -24,15 +24,39 @@
         Please enter a valid email address!
       </div>
     </div>
-    <div class="mb-3">
+    <div class="mb-3" v-if="route === 'register'">
       <label for="form_password" class="form-label">{{ form.password }}</label>
       <input type="password" class="form-control" id="form_password" placeholder="•••" name="password"
-             pattern="^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$" required>
+             pattern="^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$" @input="check_password" required>
+      <div class="valid-feedback">
+        Looks good!
+      </div>
+      <div class="invalid-feedback">
+        - At least 8 characters<br>
+        - Contains an uppercase letter, lowercase letter, and a number<br>
+        - Can contain special characters
+      </div>
+    </div>
+    <div class="mb-3" v-else>
+      <label for="form_password" class="form-label">{{ form.password }}</label>
+      <input type="password" class="form-control" id="form_password" placeholder="•••" name="password"
+             pattern="^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$" required>
       <div class="valid-feedback">
         Looks good!
       </div>
       <div class="invalid-feedback">
         Minimum eight characters, at least one letter and one number!
+      </div>
+    </div>
+    <div class="mb-3" v-if="route === 'register'">
+      <label for="form_password_confirm" class="form-label">{{ form.confirm }}</label>
+      <input type="password" class="form-control" id="form_password_confirm" placeholder="•••" name="password_confirm"
+             pattern="^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$" @input="check_password" required>
+      <div class="valid-feedback">
+        Looks good!
+      </div>
+      <div class="invalid-feedback">
+        Should be the same as the password!
       </div>
     </div>
     <button v-if="!loading.value" id="button_submit" type="submit" class="btn btn-primary form-submit"
@@ -59,16 +83,17 @@ export default {
     return {
       route_base: "api/auth/",
       head: {
-        login: "Melde dich an!",
-        register: "Registriere dich!",
+        login: "Log In!",
+        register: "Register!",
       },
       form: {
         username: "E-Mail",
         password: "Password",
+        confirm: "Retype password",
         name: "Name",
         submit: {
           name: "Submit",
-          loading: "Fertigstellen..."
+          loading: "Loading..."
         }
       }
     };
@@ -105,6 +130,15 @@ export default {
             router.push("../login");
           }
         })
+    },
+    async check_password() {
+      const password = document.getElementById("form_password") as HTMLInputElement;
+      const password_confirm = document.getElementById("form_password_confirm") as HTMLInputElement;
+      if (password.value !== password_confirm.value) {
+        password_confirm.setCustomValidity("Should be the same as the password!");
+      } else {
+        password_confirm.setCustomValidity("");
+      }
     }
   }
 };
