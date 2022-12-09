6b459957c136fe9eb451ccf9968c85ef124e55f3
EliasSchaut <eschaut@outlook.de>
2022-12-09 15:19:09 +0100

    types: add MovieAll type

---
+++ src/types/movie.types/MovieAll.ts
@@ -0,0 +1,12 @@
+
+export interface MovieAll {
+  imdb_id: string;
+  title: string,
+  link: string,
+  year: string,
+  genre: string,
+  proposer: string,
+  proposer_id: number,
+  createdAt: Date,
+  votes: number
+}
