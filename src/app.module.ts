b780b837ba30eb349f612c8f0efcc646538f92a7
EliasSchaut <eschaut@outlook.de>
2022-12-10 16:51:10 +0100

    common: replace dotenv package with @nestjs/config

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,14 +2,19 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './routes/auth/auth.module';
 import { UserModule } from './routes/user/user.module';
 import { ServeStaticModule } from "@nestjs/serve-static";
-import { join } from "path";
 import { MovieModule } from './routes/movie/movie.module';
 import { VoteModule } from './routes/vote/vote.module';
 import { EventModule } from "./common/event_service/event.module";
 import { ScheduleModule } from "@nestjs/schedule";
+import { ConfigModule } from "@nestjs/config";
+import { join } from "path";
 
 @Module({
-  imports: [AuthModule, UserModule, MovieModule, VoteModule, EventModule,
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    AuthModule, UserModule, MovieModule, VoteModule, EventModule,
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'client/dist'),
     }),
--- src/common/util_services/discord.service.ts
+++ src/common/util_services/discord.service.ts
@@ -4,12 +4,18 @@ import { Injectable } from "@nestjs/common";
 @Injectable()
 export class DiscordService {
   private readonly webhookClient;
+  private readonly enabled = process.env.DISCORD_ENABLE === 'true';
 
   constructor() {
-    this.webhookClient = new WebhookClient({ url: process.env.DISCORD_WEBHOOK_URL as string });
+    if (this.enabled) {
+      this.webhookClient = new WebhookClient({ url: process.env.DISCORD_WEBHOOK_URL as string });
+    }
   }
 
   async send_message(message: string) {
-    return this.webhookClient.send(message);
+    if (this.enabled) {
+      // @ts-ignore
+      return this.webhookClient.send(message);
+    }
   }
 }
