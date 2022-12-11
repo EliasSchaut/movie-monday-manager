0c5ebbad4e242b535b3736222059ecb468ea2fce
EliasSchaut <eschaut@outlook.de>
2022-12-11 22:49:32 +0100

    client: hotfix

--- client/src/main.ts
+++ client/src/main.ts
@@ -11,7 +11,7 @@ import en from '@/locales/en.json'
 import de from '@/locales/de.json'
 
 i18next.init({
-  lng: 'en',
+  lng: 'de',
   resources: {
     en: { translation: en },
     de: { translation: de },
--- client/src/views/LoginView.vue
+++ client/src/views/LoginView.vue
@@ -2,7 +2,7 @@
   <FormComponent :title="$t('login.title')" :route="route" :callback="callback">
     <EmailComponent />
     <PasswordComponent type="single" />
-    <router-link class="mb-3" to="/reset/">Forgot Password?</router-link>
+    <router-link class="mb-3" to="/reset/">{{ $t('login.forgot_password') }}?</router-link>
   </FormComponent>
 </template>
 
