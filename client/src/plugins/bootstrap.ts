57d14cac7e39cc3ba4dd062b631dbd45ed5f6b23
EliasSchaut <eschaut@outlook.de>
2023-01-25 23:19:33 +0100

    client: remove bootstrap-table from plugins and modify package

--- client/src/plugins/bootstrap.ts
+++ client/src/plugins/bootstrap.ts
@@ -1,10 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
-// import 'bootstrap-table/dist/bootstrap-table.min.css'
 
 import './jquery'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
-// import 'bootstrap-table/dist/bootstrap-table.min.js'
 
-// import 'bootstrap-table/dist/extensions/filter-control/bootstrap-table-filter-control.css'
-// import 'bootstrap-table/dist/extensions/filter-control/bootstrap-table-filter-control.js'
-// import 'bootstrap-table/dist/extensions/cookie/bootstrap-table-cookie.js'
