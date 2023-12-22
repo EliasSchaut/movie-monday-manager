dc2f110d8fb7ce715686db053f5fec0218bd34b7
EliasSchaut <eschaut@outlook.de>
2023-12-22 01:28:13 +0100

    Rename 'host' to 'origin' and update server decorator
    The 'host' field in the Prisma schema has been renamed to 'origin' for greater precision. In the server decorator, functionality has been added to handle the retrieval of the server id based on the origin in the request headers. Error handling is also present now, with an exception thrown when the server cannot be found. The unnecessary 'server' parameter in the 'resolve_settings' function in the server resolver file has been removed.

--- server/common/decorators/server.decorator.ts
+++ server/common/decorators/server.decorator.ts
@@ -1,11 +1,29 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 import { GqlExecutionContext } from '@nestjs/graphql';
+import { PrismaService } from '@/common/services/prisma.service';
+import { DangerException } from '@/common/exceptions/danger.exception';
+import { I18nContext } from 'nestjs-i18n';
 
 export const ServerID = createParamDecorator(
-  (data: unknown, ctx: ExecutionContext) => {
+  async (data: unknown, ctx: ExecutionContext) => {
     const gql_ctx = GqlExecutionContext.create(ctx);
     const req = gql_ctx.getContext().req;
-    // console.log(req.headers);
-    return 1;
+    const prisma = new PrismaService();
+    const server = await prisma.server.findUnique({
+      select: {
+        id: true,
+      },
+      where: {
+        origin: req.headers.origin,
+      },
+    });
+
+    if (!server) {
+      throw new DangerException(
+        I18nContext.current()!.t('server.exception.not_found'),
+      );
+    }
+
+    return server.id;
   },
 );
