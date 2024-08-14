87c2edb08f7f9eaa0b261be01a0d0e82e6c58552
EliasSchaut <eschaut@outlook.de>
2024-08-14 18:26:58 +0200

    server: refactor tmdb_api

--- server/common/services/movie_api/tmdb_api.service.ts
+++ server/common/services/movie_api/tmdb_api.service.ts
@@ -11,7 +11,8 @@ import { MovieApiSearchType } from '@/types/movie/movie_api_search.type';
 
 @Injectable()
 export class TmdbApiService implements MovieAPI {
-  private readonly api_key: string = process.env.TMDB_API_KEY as string;
+  private readonly API_KEY: string = process.env.TMDB_API_KEY as string;
+  private readonly API_BASE: string = 'https://api.themoviedb.org/3/';
   private readonly TIMEOUT_IN_MS: number = 10000;
   private readonly MAX_SEARCH_RESULTS: number = 6;
 
@@ -75,7 +76,19 @@ export class TmdbApiService implements MovieAPI {
       .map((movie: any) => new TmdbApiSearchType(movie));
   }
 
-  private fetch_timeout() {
+  private async call_tmdb_api(url: string): Promise<any> {
+    const tmdb_fetch_timeout = this.create_tmdb_fetch_timeout();
+    const res: Response = await fetch(url).catch(() => {
+      clearTimeout(tmdb_fetch_timeout);
+      throw new DangerException(
+        I18nContext.current()!.t('movie.exception.create_api_not_found'),
+      );
+    });
+    clearTimeout(tmdb_fetch_timeout);
+    return await res.json();
+  }
+
+  private create_tmdb_fetch_timeout() {
     return setTimeout(() => {
       throw new DangerException(
         I18nContext.current()!.t('movie.exception.create_api_not_found'),
@@ -83,27 +96,15 @@ export class TmdbApiService implements MovieAPI {
     }, this.TIMEOUT_IN_MS);
   }
 
-  private async call_tmdb_api(url: string): Promise<any> {
-    const fetch_timeout = this.fetch_timeout();
-    const res: Response = await fetch(url).catch(() => {
-      clearTimeout(fetch_timeout);
-      throw new DangerException(
-        I18nContext.current()!.t('movie.exception.create_api_not_found'),
-      );
-    });
-    clearTimeout(fetch_timeout);
-    return await res.json();
-  }
-
   private gen_movie_link(tmdb_id: string, lang: string = 'en-US') {
-    return `https://api.themoviedb.org/3/movie/${tmdb_id}?language=${lang}&api_key${this.api_key}`;
+    return `${this.API_BASE}movie/${tmdb_id}?language=${lang}&api_key${this.API_KEY}`;
   }
 
   private gen_credit_link(tmdb_id: string, lang: string = 'en-US') {
-    return `https://api.themoviedb.org/3/movie/${tmdb_id}/credits?language=${lang}&api_key${this.api_key}`;
+    return `${this.API_BASE}movie/${tmdb_id}/credits?language=${lang}&api_key${this.API_KEY}`;
   }
 
   private gen_movie_search_link(search_query: string, lang: string = 'en-US') {
-    return `https://api.themoviedb.org/3/search/movie?query=${search_query}&language=${lang}&include_adult=true&api_key${this.api_key}`;
+    return `${this.API_BASE}search/movie?query=${search_query}&language=${lang}&include_adult=true&api_key${this.API_KEY}`;
   }
 }
