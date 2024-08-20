ce7b8546009f42ece3f68c067fc5b00abf3405f1
EliasSchaut <eschaut@outlook.de>
2024-08-20 18:09:25 +0200

    update database and .env schema

--- server/common/validation/env.validation.ts
+++ server/common/validation/env.validation.ts
@@ -1,52 +1,72 @@
 import * as Joi from 'joi';
 
 export const EnvValidationSchema = Joi.object({
-  PORT: Joi.number()
+  PORT_FRONTEND: Joi.number()
     .default('3000')
     .port()
-    .description('The port the server should listen on'),
+    .description('The port the frontend server should listen on'),
+
+  PORT_BACKEND: Joi.number()
+    .default('3001')
+    .port()
+    .description('The port the backend server should listen on'),
 
   PORT_NEST_DEVTOOLS: Joi.number()
-    .default('3001')
+    .default('8000')
     .port()
     .description('The port for the NestJs Devtools'),
 
-  IMDB_API_KEY: Joi.string()
-    .required()
-    .pattern(/^k_.+$/)
-    .description(
-      'An API key of website https://imdb-api.com/ to retrieve movie data',
-    ),
+  FRONTEND_URL: Joi.string()
+    .default('http://localhost:3000')
+    .uri()
+    .description('The URL of the frontend server'),
+
+  BACKEND_URL: Joi.string()
+    .default('http://localhost:3001')
+    .uri()
+    .description('The URL of the backend server'),
+
   DATABASE_URL: Joi.string()
     .default('file:./dev.db')
-    .description('The URL of the database'),
+    .description(
+      'The URL of the database. Supports SQLite, MySQL, MariaDB, Postgres, and MSSQL',
+    ),
 
   JWT_SECRET: Joi.string()
     .required()
     .disallow('secret')
     .description(
-      'The secret used to sign the JWT tokens. The JWT tokens are used to authenticate the users. Should be a very long and random string',
+      'The secret used to sign the JWT tokens. The JWT tokens are used to authenticate the users. Should be a very long and random string.',
     ),
 
   JWT_EXPIRATION: Joi.string()
     .default('2h')
     .description('The expiration time of the JWT tokens'),
 
+  TMDB_API_KEY: Joi.string()
+    .optional()
+    .description('The API key for The Movie Database (TMDb)'),
+
+  OMDB_API_KEY: Joi.string()
+    .optional()
+    .description('The API key for the Open Movie Database (OMDb)'),
+
   EMAIL_HOST: Joi.string()
-    .required()
+    .optional()
     .domain()
     .description('The host of the SMTP server used to send emails'),
 
   EMAIL_PORT: Joi.number()
+    .optional()
     .default('587')
-
+    .port()
     .description('The port of the SMTP server used to send emails'),
+
   EMAIL_HOST_USER: Joi.string()
-    .required()
-    .email()
+    .optional()
     .description('The username of the SMTP server used to send emails'),
 
   EMAIL_HOST_PASSWORD: Joi.string()
-    .required()
+    .optional()
     .description('The password of the SMTP server used to send emails'),
 });
